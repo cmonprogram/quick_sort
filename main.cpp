af54d3c11fc92a879ee2351a9aca3ccc7a131290
vlad <vlad5956@gmail.com>
2023-11-20 15:28:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,45 +3,44 @@
 
 using namespace std;
 
-template<typename T, typename C = std::nullptr_t>
+template <typename T, typename C = std::nullptr_t>
+void QuickSort(std::vector<T>& arr, int low, int high, C comp = nullptr)
+{
+	if (low < high) {
+		T pivot = arr.at(high);
+		int i = low - 1;
+		for (int j = low; j <= high - 1; ++j) {
+			if constexpr (std::is_same<C, std::nullptr_t>::value) {
+				if (arr.at(j) < pivot) {
+					++i;
+					std::swap(arr.at(i), arr.at(j));
+				}
+			}
+			else {
+				if (comp(arr.at(j), pivot)) {
+					++i;
+					std::swap(arr.at(i), arr.at(j));
+				}
+			}
+		}
+		++i;
+		std::swap(arr.at(i), arr.at(high));
+
+		if constexpr (std::is_same<C, std::nullptr_t>::value) {
+			QuickSort(arr, low, i - 1);
+			QuickSort(arr, i + 1, high);
+		}
+		else {
+			QuickSort(arr, low, i - 1, comp);
+			QuickSort(arr, i + 1, high, comp);
+		}
+
+	}
+}
+
+template <typename T, typename C = std::nullptr_t>
 void QuickSort(std::vector<T>& input, C comp = nullptr) {
-
-    if (input.size() <= 1) return;
-
-    int middle = (input.size() - 1) / 2;
-    int base_elem = input[middle];
-
-    std::vector<T> v_low;
-    std::vector<T> v_high;
-
-    int i = 0;
-    for (auto&& elem : input) {
-        if (i != middle) {
-            if constexpr (std::is_same<C, std::nullptr_t>::value) {
-                if (elem <= base_elem) { v_low.push_back(std::move(elem)); }
-                else { v_high.push_back(std::move(elem)); }
-            }
-            else {
-                if (comp(elem, base_elem)) { v_low.push_back(std::move(elem)); }
-                else { v_high.push_back(std::move(elem)); }
-            }
-        }
-        ++i;
-    }
-
-    if constexpr (std::is_same<C, std::nullptr_t>::value) {
-        QuickSort(v_low);
-        QuickSort(v_high);
-    }
-    else {
-        QuickSort(v_low, comp);
-        QuickSort(v_high, comp);
-    }
-
-    v_low.push_back(std::move(base_elem));
-
-    std::move(v_low.begin(), v_low.end(), input.begin());
-    std::move(v_high.begin(), v_high.end(), input.begin() + v_low.size());
+	QuickSort(input, 0, input.size() - 1, comp);
 }
 
 int main() {
